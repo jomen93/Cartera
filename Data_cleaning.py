4f7c10a54c9e71afce7f2f2c2a271b3f98072562
Johan <jsmendezh@unal.edu.co>
2021-08-09 20:59:26 -0500

    Add first manipulation to time predict method

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -10,7 +10,8 @@
 
 import pandas as pd
 import numpy as np 
-from sklearn.preprocessing import (OneHotEncoder, StandardScaler)
+from sklearn.preprocessing import (OneHotEncoder, StandardScaler, LabelEncoder)
+from time import time 
 
 import parameter_reduction
 
@@ -107,7 +108,73 @@ def Cleaning(data):
 	return X, y
 
 
+def Cleaning_time(data):
+	# Data Modifications
+	data['PAYDAY'] = (data['FECHA_PAGO'] - data['FECHA_REGISTRO_CARTERA'])/np.timedelta64(1,'D')
+	data['FECHA_INICIO_CONTRATO'] = pd.to_datetime( data['FECHA_INICIO_CONTRATO'], format = '%Y-%m-%d' )
+	data['CONTRACT_LIFE'] = (data['FECHA_COBRO'] - data['FECHA_INICIO_CONTRATO'])/np.timedelta64(1,'D')
+
+	Ids = data[['ID_FECHA_CONSULTADA', 'NUMERO_CONTRATO', 'ID_CONTRATO', 'FECHA_REGISTRO_CARTERA', 'ID_CLIENTE']]
+	data = data.drop( columns = ['ANIO_CARTERA', 'MES_CARTERA', 'ANIOMES_CARTERA',
+	                'TIPO_IDENTIFICACION', 'IDENTIFICACION', 'FECHA_INICIO_CONTRATO', 'FECHA_REGISTRO_CARTERA',
+	                'FECHA_COBRO', 'FECHA_PAGO', 'FECHA_RECAUDO', 'ID_CLIENTE', 'EN_MORA_CONTRATO_ACTUALMENTE', 'ID_GEOGRAFIA',
+	                                            'CIUDAD', "REGION"] )
+
+	cat_variables = ['SEXO', 'REPUTACION_CLIENTE', 'GARANTIA_COLATERAL_CLIENTE', 'NIVEL_RIESGO_CLIENTE',
+	                'CAPACIDAD_CLIENTE', 'CAPITAL_CLIENTE', 'PROVINCIA_ESTADO_DEPARTAMENTO',
+	                'EN_MORA_CONTRATO_ALGUNA_VEZ' ]
 
 
+	print( 'Encoding categorical variables ...' )
+	t1 = time()
+	for some_feat in cat_variables:
+		some_labenc = LabelEncoder()
+		data[some_feat] = data[some_feat].fillna( 'OTRO' )
+		data[some_feat] = some_labenc.fit_transform( data[some_feat] )
+		data = pd.concat( [data, pd.get_dummies( data[some_feat], prefix = some_feat )], axis = 1 )
+		data = data.drop( columns = some_feat ) 
+	t2 = time()
+
+	print( 'Encoding finished. Time = {:.2f} sec'.format(t2 - t1) )
+    	
+	data['Nivel_Retraso'] = 'Baja'
+	data.at[data['PAYDAY'] > 30, 'Nivel_Retraso'] = 'Media'
+	data.at[data['PAYDAY'] > 60, 'Nivel_Retraso'] = 'Alta'
+
+	data = data.reset_index().drop( columns = 'index' ).reset_index().rename( columns = {'index' : 'Id_Row'} )
+
+	data.at[ data['PAYDAY'] > 90, 'PAYDAY' ] = 90
+
+	data = data[ ~data['PAYDAY'].isna() ]
+	data['PAYDAY'] = data['PAYDAY'].astype('int')
+	data_low = data[ data['Nivel_Retraso'] == 'Baja' ]
+	data_mid = data[ data['Nivel_Retraso'] == 'Media' ]
+	data_high = data[ data['Nivel_Retraso'] == 'Alta' ]
+    
+	data_low_train = data_low.sample( 850 )
+	data_mid_train = data_mid.sample( 850 )
+	data_high_train = data_high.sample( 850 )
+    
+	data_train = pd.concat( [data_low_train, data_mid_train, data_high_train], axis = 0 )
+	data_train = data_train.sample( frac = 1 )
+	data_test = data[ ~data['Id_Row'].isin( data_train['Id_Row'] ) ]
+    
+	Id_train = data_train[['Id_Row', 'ID_FECHA_CONSULTADA', 'NUMERO_CONTRATO', 'ID_CONTRATO']]
+	Y_train = data_train[['Nivel_Retraso', 'PAYDAY']]
+	X_train = data_train.drop( columns = ['Id_Row', 'ID_FECHA_CONSULTADA', 'NUMERO_CONTRATO',
+                                          'ID_CONTRATO', 'Nivel_Retraso', 'PAYDAY'] )
+	Id_test = data_test[['Id_Row', 'ID_FECHA_CONSULTADA', 'NUMERO_CONTRATO', 'ID_CONTRATO']]
+	Y_test = data_test[['Nivel_Retraso', 'PAYDAY']]
+	X_test = data_test.drop( columns = ['Id_Row', 'ID_FECHA_CONSULTADA', 'NUMERO_CONTRATO',
+                                          'ID_CONTRATO', 'Nivel_Retraso', 'PAYDAY'] )
+    
+	Id_train = Id_train.reset_index().drop( columns = 'index' )
+	Y_train = Y_train.reset_index().drop( columns = 'index' )
+	X_train = X_train.reset_index().drop( columns = 'index' )
+	Id_test = Id_test.reset_index().drop( columns = 'index' )
+	Y_test = Y_test.reset_index().drop( columns = 'index' )
+	X_test = X_test.reset_index().drop( columns = 'index' )
+
+	return Id_train, X_train, Y_train, Id_test, X_test, Y_test
 
 
